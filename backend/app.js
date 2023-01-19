3fad78b6a1f1a269327dcd9938cfff2db1b04352
sucredoux <sakharovaes@yandex.ru>
2023-01-18 22:02:45 -0500

    Change port

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 const routes = require('./routes/index');
 const errorHandler = require('./middlewares/err-handler');
 
-const { PORT = 3001, MONGO_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
+const { PORT = 3000, MONGO_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 
 const app = express();
 app.use(cookieParser());
